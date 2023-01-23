80b9ecdb06990a7670f61f664b14dd168a98dc01
Craig <craigpinto17@gmail.com>
2023-01-23 12:12:05 -0500

    Update G-Analytics tag for Dev

--- views/header.php
+++ views/header.php
@@ -4,14 +4,14 @@
 
     <!-- Google Analytics -->
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZX4TRYBMWB"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3Q5C6Q2BL6"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-ZX4TRYBMWB');
+        gtag('config', 'G-3Q5C6Q2BL6');
     </script>
-
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
